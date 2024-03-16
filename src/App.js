974f2464d3c315a04086d966644da69a3c90a6c0
c-robinson23 <147357577+c-robinson23@users.noreply.github.com>
2024-03-16 16:55:44 -0400

    Testing to see if bootstrap works correctly in App.js file

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
-import Form from "./Form";
-
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h1>Hello React World</h1>
-        <Form />
+        <div class="alert alert-primary" role="alert">
+          <p style={{ display: "none" }} className="d-block">
+            Bootstrap is now successfully installed 😃
+          </p>
+          <p className="d-none">
+            Bootstrap is not installed if you can see this 😢
+          </p>
+        </div>
       </header>
     </div>
   );
