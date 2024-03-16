ce0a95750f1cd96f45aeded03d7f3af43056385c
c-robinson23 <147357577+c-robinson23@users.noreply.github.com>
2024-03-16 19:54:33 -0400

    Updated App.js with footer

--- src/App.js
+++ src/App.js
@@ -8,6 +8,32 @@ function App() {
       <header className="App-header">
         <h1>Weather App</h1>
         <SearchEngine />
+        <footer>
+          This project was coded by
+          <a
+            href="https://github.com/c-robinson23"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Candace Robinson
+          </a>
+          , is
+          <a
+            href="https://github.com/c-robinson23/react-weather-forecast-app"
+            target="_blank"
+            rel="noreferrer"
+          >
+            open-sourced on GitHub
+          </a>
+          and
+          <a
+            href="https://aesthetic-ganache-13d759.netlify.app/"
+            target="_blank"
+            rel="noreferrer"
+          >
+            hosted on Netlify
+          </a>
+        </footer>
       </header>
     </div>
   );
