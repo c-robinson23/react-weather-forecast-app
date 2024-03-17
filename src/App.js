6b403224773df2b0b477641353fdf866b3b8206b
c-robinson23 <147357577+c-robinson23@users.noreply.github.com>
2024-03-17 13:41:12 -0400

    created Weather.js file
    disabled the SearchEngine.js file.

--- src/App.js
+++ src/App.js
@@ -1,40 +1,42 @@
-import SearchEngine from "./SearchEngine";
+import Weather from "./Weather";
 
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <h1>React Weather App</h1>
-        <SearchEngine />
-        <footer>
-          This project was coded by{" "}
-          <a
-            href="https://github.com/c-robinson23"
-            target="_blank"
-            rel="noreferrer"
-          >
-            Candace Robinson
-          </a>
-          , is{" "}
-          <a
-            href="https://github.com/c-robinson23/react-weather-forecast-app"
-            target="_blank"
-            rel="noreferrer"
-          >
-            open-sourced on GitHub{" "}
-          </a>
-          and{" "}
-          <a
-            href="https://aesthetic-ganache-13d759.netlify.app/"
-            target="_blank"
-            rel="noreferrer"
-          >
-            hosted on Netlify
-          </a>
-        </footer>
-      </header>
+      <div className="container">
+        {" "}
+        <header className="App-header">
+          <Weather />
+          <footer>
+            This project was coded by{" "}
+            <a
+              href="https://github.com/c-robinson23"
+              target="_blank"
+              rel="noreferrer"
+            >
+              Candace Robinson
+            </a>
+            , is{" "}
+            <a
+              href="https://github.com/c-robinson23/react-weather-forecast-app"
+              target="_blank"
+              rel="noreferrer"
+            >
+              open-sourced on GitHub{" "}
+            </a>
+            and{" "}
+            <a
+              href="https://aesthetic-ganache-13d759.netlify.app/"
+              target="_blank"
+              rel="noreferrer"
+            >
+              hosted on Netlify
+            </a>
+          </footer>
+        </header>
+      </div>
     </div>
   );
 }
