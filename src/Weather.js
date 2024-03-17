837bcb10dfb6d6bd315d8c5b508d39f500a32be5
c-robinson23 <147357577+c-robinson23@users.noreply.github.com>
2024-03-17 16:11:18 -0400

    Updated Weather.js with HTML skeleton

--- src/Weather.js
+++ src/Weather.js
@@ -1,5 +1,45 @@
 import React from "react";
+import "./Weather.css";
 
 export default function Weather() {
-  return <h1>Weather App </h1>;
+  return (
+    <div className="Weather">
+      <form>
+        <div className="row">
+          <div className="col-9">
+            <input
+              type="search"
+              placeholder="Enter a city..."
+              className="form-control"
+            />
+          </div>
+          <div className="col-3">
+            <input type="submit" value="Submit" className="btn btn-primary" />
+          </div>
+        </div>
+      </form>
+
+      <h1>New York</h1>
+      <ul>
+        <li>Sunday 15:00</li>
+        <li>Partly Cloudy</li>
+      </ul>
+      <div className="row">
+        <div className="col-6">
+          <img
+            src="https://ssl.gstatic.com/onebox/weather/64/partly_cloudy.png"
+            alt="weather icon"
+          />
+          62˚F
+        </div>
+        <div className="col-6">
+          <ul>
+            <li>Precipitation: 0%</li>
+            <li>Humidity: 25%</li>
+            <li>Wind: 18 mph</li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  );
 }
