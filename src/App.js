692555622403c5a24b5cff5e9c20e020024d508e
c-robinson23 <147357577+c-robinson23@users.noreply.github.com>
2024-03-16 20:01:57 -0400

    Update App.js file

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h1>Weather App</h1>
+        <h1>React Weather App</h1>
         <SearchEngine />
         <footer>
           This project was coded by{" "}
