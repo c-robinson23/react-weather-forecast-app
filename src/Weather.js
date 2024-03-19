125bfe10f9ed11c159664c7a09dfb6de0bf63bee
c-robinson23 <147357577+c-robinson23@users.noreply.github.com>
2024-03-19 17:26:22 -0400

    reduced the number of useStates

--- src/Weather.js
+++ src/Weather.js
@@ -3,12 +3,12 @@ import axios from "axios";
 import "./Weather.css";
 
 export default function Weather() {
-  const [ready, setReady] = useState(false);
-  const [weatherData, setWeatherData] = useState({});
+  const [weatherData, setWeatherData] = useState({ ready: false });
 
   function handleResponse(response) {
     console.log(response.data);
     setWeatherData({
+      ready: true,
       temperature: response.data.main.temp,
       humidity: response.data.main.humidity,
       date: "Sunday 15:00",
@@ -17,11 +17,9 @@ export default function Weather() {
       wind: response.data.wind.speed,
       city: response.data.name,
     });
-
-    setReady(true);
   }
 
-  if (ready) {
+  if (weatherData.ready) {
     return (
       <div className="Weather">
         <form>
