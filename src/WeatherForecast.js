044149e2693daa4cead07650b0bd3d60fc4cb379
c-robinson23 <147357577+c-robinson23@users.noreply.github.com>
2024-03-27 19:54:14 -0400

    Added WeatherForecast.js and WeatherForecast.css file
    1 Day forecast template created

---
+++ src/WeatherForecast.js
@@ -0,0 +1,24 @@
+import React from "react";
+import WeatherIcon from "./WeatherIcon";
+
+import "./WeatherForecast.css";
+
+export default function WeatherForecast() {
+  return (
+    <div className="WeatherForecast">
+      <div className="row">
+        <div className="col">
+          <div className="WeatherForecast-day">Thu</div>
+          <div className="WeatherForecast-icon">
+            <WeatherIcon code="01d" size={25} />
+          </div>
+
+          <div className="WeatherForecast-temperatures">
+            <span className="WeatherForecast-temperature-max">19˚</span>
+            <span className="WeatherForecast-temperature-min">10˚</span>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
