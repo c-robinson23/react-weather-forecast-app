d76958698dc049a381962e45b9f046713de56424
c-robinson23 <147357577+c-robinson23@users.noreply.github.com>
2024-03-27 17:08:09 -0400

    Update WeatherTemperature.js component
    removed the conversion of temperature unit option

--- src/WeatherTemperature.js
+++ src/WeatherTemperature.js
@@ -1,44 +1,10 @@
-import React, { useState } from "react";
+import React from "react";
 
 export default function WeatherTemperature(props) {
-  const [unit, setUnit] = useState("fahrenheit");
-  function showCelsius(event) {
-    event.preventDefault();
-    setUnit("celsius");
-  }
-
-  function showFahrenheit(event) {
-    event.preventDefault();
-    setUnit("fahrenheit");
-  }
-
-  function celsius() {
-    return (5 / 9) * (props.fahrenheit - 32);
-  }
-
-  if (unit === "fahrenheit") {
-    return (
-      <span className="WeatherTemperature">
-        <span className="temperature">{Math.round(props.fahrenheit)}</span>
-        <span className="unit">
-          ˚F |{" "}
-          <a href="/" onClick={showCelsius}>
-            ˚C
-          </a>
-        </span>
-      </span>
-    );
-  } else {
-    return (
-      <span className="WeatherTemperature">
-        <span className="temperature">{Math.round(celsius())}</span>
-        <span className="unit">
-          <a href="/" onClick={showFahrenheit}>
-            ˚F
-          </a>{" "}
-          | ˚C
-        </span>
-      </span>
-    );
-  }
+  return (
+    <span className="WeatherTemperature">
+      <span className="temperature">{Math.round(props.fahrenheit)}</span>
+      <span className="unit">˚F</span>
+    </span>
+  );
 }
