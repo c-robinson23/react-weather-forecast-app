39e5f300e8e9d036ade6dc214058413a9a075b4a
c-robinson23 <147357577+c-robinson23@users.noreply.github.com>
2024-03-28 13:14:14 -0400

    Updated to test AJAX for a city's coordinates

--- src/WeatherForecast.js
+++ src/WeatherForecast.js
@@ -1,9 +1,20 @@
 import React from "react";
 import WeatherIcon from "./WeatherIcon";
-
+import axios from "axios";
 import "./WeatherForecast.css";
 
 export default function WeatherForecast() {
+  function handleResponse(response) {
+    console.log(response.data);
+  }
+
+  let apiKey = "2b6fdad0cbd018949c50c70f72250726";
+  let longitude = 40.7;
+  let latitude = 74;
+  let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=74&lon=40.7&appid=2b6fdad0cbd018949c50c70f72250726&units=imperial`;
+
+  axios.get(apiUrl).then(handleResponse);
+
   return (
     <div className="WeatherForecast">
       <div className="row">
